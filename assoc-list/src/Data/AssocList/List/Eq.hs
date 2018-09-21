af5966f42fb679beb290e68f3c67df35973bd705
Chris Martin <ch.martin@gmail.com>
2018-09-21 15:04:46 -0400

    add lookup/remove functions for Eq

--- assoc-list/src/Data/AssocList/List/Eq.hs
+++ assoc-list/src/Data/AssocList/List/Eq.hs
@@ -18,3 +18,27 @@ import Prelude (Eq (..), Maybe (..), error, otherwise)
 ((x, y) : xys) !? key
         | key == x                 = Just y
         | otherwise                = xys !? key
+
+lookupFirst :: Eq a => a -> AssocList a b -> Maybe b
+lookupFirst _key []                =  Nothing
+lookupFirst key ((x, y) : xys)
+        | key == x                 =  Just y
+        | otherwise                =  lookupFirst key xys
+
+lookupAll :: Eq a => a -> AssocList a b -> [b]
+lookupAll _key []                  =  []
+lookupAll key ((x, y) : xys)
+        | key == x                 =  y : lookupAll key xys
+        | otherwise                =      lookupAll key xys
+
+removeFirst :: Eq a => a -> AssocList a b -> AssocList a b
+removeFirst _key l@[]              =  l
+removeFirst key (xy@(x, y) : xys)
+        | key == x                 =  xys
+        | otherwise                =  xy : removeFirst key xys
+
+removeAll :: Eq a => a -> AssocList a b -> AssocList a b
+removeAll _key l@[]                =  l
+removeAll key (xy@(x, y) : xys)
+        | key == x                 =       removeAll key xys
+        | otherwise                =  xy : removeAll key xys
